aa50fb8807b06b845077bca2afa64a9967b8cd60
Daniel Lima <danielunivap@outlook.com>
2021-11-04 16:12:09 -0300

    Login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,28 @@
 <template>
-    <div>
-        <h1>Login</h1>
-    </div>
+    <div class="corda"></div>
+  <div class="card">
+    <div class="risco"></div>
+        <div class="card-img">
+            <img src="../assets/logo.png">
+        </div>
+        <div class="desc">
+            <h6 class="primary-text">Daniel Lima</h6>
+            <h6 class="secondary-text">Desenvolvedor Nubank</h6>
+        </div>
+         
+        
+         <button class="primary-text">Frontend Pleno</button>
+        <div class="details">
+            <div class="rating">
+                <h6 class="primary-text"> 15 </h6>
+                <h6 class="secondary-text"> Projetos </h6>
+            </div>
+            <div class="activity">
+                <h6 class="primary-text"> 92% </h6>
+                <h6 class="secondary-text"> Ativo </h6>
+            </div>
+        </div>
+</div>
 </template>
 
 <script>
@@ -11,3 +32,7 @@ export default {
     name: 'Login'
 }
 </script>
+
+<style scoped src="../assets/css/main.css">
+    
+</style>
