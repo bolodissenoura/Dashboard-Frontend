50b4ab6419c93125860587f60f28238a73718128
Daniel Lima <danielunivap@outlook.com>
2021-11-06 15:50:20 -0300

    Login section

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,7 @@
   <div class="card">
     <div class="risco"></div>
         <div class="card-img">
-            <img src="../assets/logo.png">
+            <img class="img-logo" src="../assets/logo.png">
         </div>
         <div class="desc">
             <h6 class="primary-text">Daniel Lima</h6>
@@ -11,16 +11,8 @@
         </div>
          
         
-         <button class="primary-text">Frontend Pleno</button>
         <div class="details">
-            <div class="rating">
-                <h6 class="primary-text"> 15 </h6>
-                <h6 class="secondary-text"> Projetos </h6>
-            </div>
-            <div class="activity">
-                <h6 class="primary-text"> 92% </h6>
-                <h6 class="secondary-text"> Ativo </h6>
-            </div>
+            <a href=""><h5>Esqueci minha senha</h5>  </a>          
         </div>
 </div>
 </template>
